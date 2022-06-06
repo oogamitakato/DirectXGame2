1588685de6841eb673ad4b07675985b09cb0c8ae
TakatoOogami <k021g1156@g.neec.ac.jp>
2022-06-06 09:46:27 +0900

    re02_01

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -43,41 +43,6 @@ void GameScene::Initialize() {
 	//ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
 	PrimitiveDrawer::GetInstance()->SetViewProjection(&debugCamera_->GetViewProjection());
 
-	for (int i = 0; i < 8; i++) 
-	{
-		//平行移動する立方体の初期化
-		boxMoved[i] = vertex[i];
-		boxMoved2[i] = boxMoved[i];
-
-		boxMoved[i] = {
-		  boxMoved2[i].x * position[0][0] + boxMoved2[i].y * position[1][0] + boxMoved2[i].z * position[2][0] + position[3][0],
-		  boxMoved2[i].x * position[0][1] + boxMoved2[i].y * position[1][1] + boxMoved2[i].z * position[2][1] + position[3][1],
-		  boxMoved2[i].x * position[0][2] + boxMoved2[i].y * position[1][2] + boxMoved2[i].z * position[2][2] + position[3][2],
-		  
-		};
-
-		//回転する立方体の初期化
-		boxRotated[i] = vertex[i];
-		boxRotated2[i] = boxRotated[i];
-
-		boxRotated[i] = {
-		  boxRotated2[i].x * rotation[0][0] + boxRotated2[i].y * rotation[1][0] + boxRotated2[i].z * rotation[2][0],
-		  boxRotated2[i].x * rotation[0][1] + boxRotated2[i].y * rotation[1][1] + boxRotated2[i].z * rotation[2][1],
-		  boxRotated2[i].x * rotation[0][2] + boxRotated2[i].y * rotation[1][2] + boxRotated2[i].z * rotation[2][2],
-		};
-
-		//拡大する立方体の初期化
-		boxScaled[i] = vertex[i];
-		boxScaled2[i] = boxScaled[i];
-
-		boxScaled[i] = {
-		  boxScaled2[i].x * scale[0][0] + boxScaled2[i].y * scale[1][0] + boxScaled2[i].z * scale[2][0],
-		  boxScaled2[i].x * scale[0][1] + boxScaled2[i].y * scale[1][1] + boxScaled2[i].z * scale[2][1],
-		  boxScaled2[i].x * scale[0][2] + boxScaled2[i].y * scale[1][2] + boxScaled2[i].z * scale[2][2],
-		};
-	}
-
-
 }
 
 void GameScene::Update() { 
@@ -114,7 +79,7 @@ void GameScene::Draw() {
 	/// </summary>
 
 	//3Dモデル描画
-	//model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
+	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
@@ -128,20 +93,6 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-
-	//ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
-	for (int i = 0; i < 12; i++) 
-	{
-		PrimitiveDrawer::GetInstance()->DrawLine3d(
-		  vertex[eageList[i][0]], vertex[eageList[i][1]], Vector4(255, 255, 255, 255));
-		PrimitiveDrawer::GetInstance()->DrawLine3d(
-		  boxMoved[eageList[i][0]], boxMoved[eageList[i][1]], Vector4(255, 0, 0, 255));
-		PrimitiveDrawer::GetInstance()->DrawLine3d(
-		  boxRotated[eageList[i][0]], boxRotated[eageList[i][1]], Vector4(0, 255, 0, 255));
-		PrimitiveDrawer::GetInstance()->DrawLine3d(
-		  boxScaled[eageList[i][0]], boxScaled[eageList[i][1]], Vector4(0, 0, 255, 255));
-	}
-
 
 	// デバッグテキストの描画
 	debugText_->DrawAll(commandList);
