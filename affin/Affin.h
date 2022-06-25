926c50c9443c36eaf4dedc6a7274badcf90ee738
TakatoOogami <k021g1156@g.neec.ac.jp>
2022-06-25 22:08:09 +0900

    弾が出るようになった

---
+++ affin/Affin.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Model.h"
+#include "WorldTransform.h"
+
+//スケーリング設定
+void ConversionScale(float& x, float& y, float& z, WorldTransform worldTransform);
+
+//回転角設定
+void ConversionRot(float& x, float& y, float& z, WorldTransform worldTransform);
+
+//平行移動設定
+void ConversionTrans(float x, float y, float z,WorldTransform worldTransform);
